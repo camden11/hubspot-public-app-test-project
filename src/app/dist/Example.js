89586c74645d888b67ae1d755ce02d0302a3f585
Camden Phalen <cphalen@hubspot.com>
2024-05-17 15:49:52 -0400

    test

---
+++ src/app/dist/Example.js
@@ -0,0 +1,8 @@
+(function(React2, uiExtensions2) {
+  "use strict";
+  uiExtensions2.hubspot.extend(({ context }) => /* @__PURE__ */ React2.createElement(Hello, { context }));
+  const Hello = ({ context }) => {
+    return /* @__PURE__ */ React2.createElement(uiExtensions2.Text, null, "Hello world");
+  };
+})(React, uiExtensions);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRXhhbXBsZS5qcyIsInNvdXJjZXMiOlsiLi4vZXh0ZW5zaW9ucy9FeGFtcGxlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJcbmltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCc7XG5pbXBvcnQgeyBUZXh0IH0gZnJvbSAnQGh1YnNwb3QvdWktZXh0ZW5zaW9ucyc7XG5pbXBvcnQgeyBodWJzcG90IH0gZnJvbSAnQGh1YnNwb3QvdWktZXh0ZW5zaW9ucyc7XG5cbmh1YnNwb3QuZXh0ZW5kKCh7Y29udGV4dH0pID0+ICg8SGVsbG8gY29udGV4dD17Y29udGV4dH0vPikpO1xuXG5jb25zdCBIZWxsbyA9ICh7IGNvbnRleHQgfSkgPT4ge1xuICByZXR1cm4gKFxuICAgIDxUZXh0PkhlbGxvIHdvcmxkPC9UZXh0PlxuICApO1xufTtcbiJdLCJuYW1lcyI6WyJodWJzcG90IiwiUmVhY3QiLCJUZXh0Il0sIm1hcHBpbmdzIjoiOztBQUtBQSxFQUFBQSxjQUFBQSxRQUFRLE9BQU8sQ0FBQyxFQUFDLGNBQWUsZ0JBQUFDLE9BQUEsY0FBQSxPQUFBLEVBQU0sUUFBaUIsQ0FBQSxDQUFHO0FBRTFELFFBQU0sUUFBUSxDQUFDLEVBQUUsY0FBYztBQUUzQixXQUFBLGdCQUFBQSxPQUFBLGNBQUNDLDBCQUFLLGFBQVc7QUFBQSxFQUVyQjs7In0=
